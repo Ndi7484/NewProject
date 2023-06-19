758ef19a910809abd5555fa1419dac595c16154d
Ndimas Radatime <dimasradatime@gmail.com>
2023-06-20 00:41:56 +0700

    ndimas add history orders

--- lib/features/history_orders/widgets/order_card.dart
+++ lib/features/history_orders/widgets/order_card.dart
@@ -18,14 +18,61 @@ class OrderCard extends StatefulWidget {
 class _OrderCardState extends State<OrderCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-      ),
-      child: ListTile(
-        leading: Image.asset('assets/etc/Delivery.png'),
-        title: Text('Hello'),
-        subtitle: Text('hello'),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: 80,
+        decoration: const BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(
+              color: Colors.grey, offset: Offset(2.0, 3.0), blurRadius: 4.0)
+        ]),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                (widget.type == TypeOrder.delivery)
+                    ? 'assets/etc/Delivery.png'
+                    : (widget.type == TypeOrder.takeaway)
+                        ? 'assets/etc/Takeaway.png'
+                        : 'assets/etc/DineIn.png',
+                width: 60,
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 4.0, 4.0, 4.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'D${widget.indexing}',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 14),
+                  ),
+                  RichText(
+                      textAlign: TextAlign.justify,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      text: TextSpan(
+                        text: widget.data.deliveryAddress?.alamatLengkap,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 10,
+                        ),
+                      )),
+                  const SizedBox(
+                    height: 3,
+                  ),
+                  const Spacer(),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
